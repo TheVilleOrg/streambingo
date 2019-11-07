72b17b8929ccb4ab44876381f261ba89cf2e89a9
Steve Guidetti <stevotvr@gmail.com>
2019-11-07 15:14:03 -0800

    Split source view CSS into new file

--- src/inc/views/_header.php
+++ src/inc/views/_header.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<link rel="stylesheet" href="<?php echo $basePath; ?>css/bingo.min.css">
+	<link rel="stylesheet" href="<?php echo $basePath; ?>css/main.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.slim.js"></script>
 <?php if (isset($scripts)): ?>
