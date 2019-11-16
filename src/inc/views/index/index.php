27a1a5d77f5a123e70b063396a3183fda8c6ca84
Steve Guidetti <stevotvr@gmail.com>
2019-11-16 13:26:34 -0800

    Update intro text

--- src/inc/views/index/index.php
+++ src/inc/views/index/index.php
@@ -1,6 +1,7 @@
 <?php require __DIR__ . '/../_header.php'; ?>
-		<h2>Welcome to Stream BINGO</h2>
-        <p>Stream BINGO is a game made for live streaming.</p>
+		<h2>Welcome to Stream <span class="letter-b">B</span><span class="letter-i">I</span><span class="letter-n">N</span><span class="letter-g">G</span><span class="letter-o">O</span></h2>
+        <p>Stream <span class="letter-b">B</span><span class="letter-i">I</span><span class="letter-n">N</span><span class="letter-g">G</span><span class="letter-o">O</span> is a game made for live streaming. It is currently in beta so there may be bugs.</p>
+        <p>Visit one of the Twitch channels below and say <strong>!play</strong> in chat to join the fun!</p>
 		<table id="game-list">
 			<tr>
 				<th>Game</th>
