69a772036840caef9d9243fa7adacd178d1c6e7d
Steve Guidetti <stevotvr@gmail.com>
2019-11-18 15:04:12 -0800

    Update copyright notice

--- src/inc/views/_footer.php
+++ src/inc/views/_footer.php
@@ -1,5 +1,5 @@
 	</div>
-    <div id="footer"><small>Copyright &copy; 2019</small></div>
+    <div id="footer"><small>Copyright &copy; 2019 UltraMega Software</small></div>
     <div id="beta-notice">BETA</div>
     <div id="version"><?php echo $app['version']['string']; ?></div>
 </body>
